92375a78f43d5f62c3036b00a79f78200714e672
Charles Cabergs <me@cacharle.xyz>
2021-01-09 00:25:28 +0100

    Added code interpolation

--- README.md
+++ README.md
@@ -5,7 +5,12 @@ Common Listp templating language.
 ## Usage
 
 ```html
-{% (loop :for i in '(1 2 3)) %}
-    <li> {{ i }} </li>
-{% ) %}
+<% (loop :for i in '(1 2 3)) %>
+    <li> <%= i %> </li>
+<% ) %>
 ```
+
+## TODO
+
+* [x] value interpolation (e.g `<%= link %>`)
+* [ ] insert variable in local scope
--- temper.lisp
+++ temper.lisp
@@ -8,17 +8,24 @@
 (defconstant +temper-open+ "<%")
 (defconstant +temper-close+ "%>")
 (defconstant +temper-close-len+ (length +temper-close+))
+(defconstant +temper-interpolate+ #\=)
 
 (defun lex (input)
   (let ((pos (search +temper-open+ input)))
     (if (null pos)
       (list (list 'string input))
-      (let* ((before    (subseq input 0 pos))
-             (input     (subseq input pos))
-             (end-pos   (search +temper-close+ input))
-             (code      (subseq input +temper-close-len+ end-pos))
-             (input     (subseq input (+ end-pos +temper-close-len+))))
-        (append (list (list 'string before) (list 'code code)) (lex input))))))
+      (let* ((before  (subseq input 0 pos))
+             (input   (subseq input pos))
+             (end-pos (search +temper-close+ input))
+             (code    (subseq input +temper-close-len+ end-pos))
+             (input   (subseq input (+ end-pos +temper-close-len+))))
+        (append
+          (list
+            (list 'string before)
+            (if (equal (char code 0) +temper-interpolate+)
+              (list 'interpolation (subseq code 1))
+              (list 'code code)))
+          (lex input))))))
 
 
 (defun tokens-to-code-string (tokens)
@@ -28,14 +35,22 @@
       #'(lambda (token)
          (let ((token-type    (first token))
                (token-content (second token)))
-             (if (eql token-type 'string)
-               (format nil "~S"
-                 `(setf buf (concatenate
-                              'string
-                              buf
-                             ,(string-trim '(#\linefeed #\space) token-content)
-                             '(#\linefeed))))
-                token-content)))
+             (cond
+               ((eql token-type 'code) token-content)
+               ((eql token-type 'interpolation)
+                (format nil "~S"
+                  `(setf buf
+                    (concatenate 'string
+                                 buf
+                                 (format nil "~A" ,(read-from-string token-content))
+                                 '(#\linefeed)))))
+               ((eql token-type 'string)
+                (format nil "~S"
+                  `(setf buf
+                    (concatenate 'string
+                                 buf
+                                 ,(string-trim '(#\linefeed #\space) token-content)
+                                 '(#\linefeed))))))))
       tokens)))
 
 (defun generate (tokens)
@@ -48,15 +63,6 @@
         ")")))))
 
 
-
+; (format t "~A" (tokens-to-code-string (lex (read-all))))
 (setq res (generate (lex (read-all))))
 (format t "~A" (eval res))
-; (setq str (format t "~{~S~^ ~}" res))
-
-
-
-; (princ str)
-
-; (print (eval (read-from-string str)))
-
-; (read-from-string (lex (read-all)))
